e448a4f0086cc187b80667727c4ccb5e58c68ee5
Jonathan Copeland <64856330+JC2295@users.noreply.github.com>
2024-04-10 11:17:47 +0100

    Create script.js
    Added script file with basic variables and on click function.

---
+++ script.js
@@ -0,0 +1,11 @@
+let item_list = document.querySelector("#item-list");
+
+let text_input = document.querySelector("#enter-item-text");
+
+let add_button = document.querySelector("#add-item-button");
+
+add_button.addEventListener("click", addItem);
+
+function addItem(){
+    
+}
