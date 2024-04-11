c9ee3ba03f571814403abc12aa216566262723c0
Jonathan Copeland <64856330+JC2295@users.noreply.github.com>
2024-04-11 13:48:18 +0100

    Update script.js
    Added js functionality

--- script.js
+++ script.js
@@ -7,5 +7,32 @@ let add_button = document.querySelector("#add-item-button");
 add_button.addEventListener("click", addItem);
 
 function addItem(){
+
+    let text_input_value = text_input.value;
+
+    text_input.value = "";
+
+    let list_item = document.createElement("li");
+
+    let list_item_text = document.createElement("span");
+
+    let delete_item_button = document.createElement("button");
+
+    delete_item_button.innerText = "Delete";
+
+    delete_item_button.addEventListener("click", deleteItem);
+
+    list_item_text.innerText = text_input_value;
+
+    list_item.appendChild(list_item_text);
+
+    list_item.appendChild(delete_item_button);
+
+    item_list.appendChild(list_item);
+
+}
+
+
+function deleteItem(){
     
 }
